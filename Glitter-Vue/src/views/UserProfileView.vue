6c2009e60e88f800398d8cc41142c56902fddc31
SilP <113184966+SilviaPescador@users.noreply.github.com>
2025-11-28 16:56:39 +0100

    Template fix

--- Glitter-Vue/src/views/UserProfileView.vue
+++ Glitter-Vue/src/views/UserProfileView.vue
@@ -1,14 +1,19 @@
 <template>
-  <header>
-    Profile
-  </header>
-  <UserCard :username="username" :email="email" :btn="btn" :following="following.length" />
+  <div>
+    <header>Profile</header>
+    <UserCard
+      :username="username"
+      :email="email"
+      :btn="btn"
+      :following="following.length"
+    />
+  </div>
 </template>
 
 <script>
 import UserCard from "../components/UserCard.vue";
 import glitterApi from "../api/glitterApi";
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 export default {
   name: "UserProfileView",
@@ -16,17 +21,17 @@ export default {
     UserCard,
   },
   setup() {
-    const username = ref('');
-    const email = ref('');
-    const following = ref('');
+    const username = ref("");
+    const email = ref("");
+    const following = ref("");
     const btn = ref({
-      text: 'Unsubscribe',
-      class: 'btn btn-primary btn-rounded btn-lg',
-      route: "/unsubscribe"
+      text: "Unsubscribe",
+      class: "btn btn-primary btn-rounded btn-lg",
+      route: "/unsubscribe",
     });
 
     const getUser = async () => {
-      const response = await glitterApi.get('/users');
+      const response = await glitterApi.get("/users");
       const user = response.data.data;
       username.value = user.username;
       email.value = user.email;
@@ -51,7 +56,6 @@ export default {
 header {
   display: flex;
   justify-content: center;
-  align-items: center;
   align-items: center;
   text-shadow: 3px 3px #95a4ff;
   -webkit-text-stroke: 1px rgba(0, 0, 0);
